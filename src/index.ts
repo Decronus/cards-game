5f53db610295acfd07e7e66d45cc20ae9eea3f45
Алексей Зотин <decronus@Air-Aleksej.netis.cc>
2022-09-21 23:31:42 +0200

    ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 /* eslint-disable no-unused-vars */
 import { cards } from './cards.js';
-// import '../style.css';
+import '../style.css';
+import '/public/game-field.json';
 
-const difficultWrapItem = document.querySelectorAll('.difficult-wrap__item');
-const start = document.querySelector('.start');
+const difficultWrapItem: NodeListOf<Element> = document.querySelectorAll(
+    '.difficult-wrap__item'
+);
+const start = <HTMLElement>document.querySelector('.start');
 
-let difficultChoice;
+let difficultChoice: number;
 difficultWrapItem.forEach((el) => {
     el.addEventListener('click', (event) => {
-        const target = event.target;
+        const target = <HTMLElement>event.target;
         difficultWrapItem.forEach((elem) => {
             elem.classList.remove('selected');
         });
@@ -20,9 +23,9 @@ difficultWrapItem.forEach((el) => {
 
 // eslint-disable-next-line no-unused-vars
 
-const fillGameFieldShirt = (difficult) => {
-    const timeSec = document.querySelector('.time-sec');
-    const timeMin = document.querySelector('.time-min');
+const fillGameFieldShirt = (difficult: number) => {
+    const timeSec = <HTMLElement>document.querySelector('.time-sec');
+    const timeMin = <HTMLElement>document.querySelector('.time-min');
     let minutes = 0;
     let seconds = 0;
     const timerFunc = () => {
@@ -30,7 +33,7 @@ const fillGameFieldShirt = (difficult) => {
         if (seconds < 10) {
             timeSec.textContent = '0' + seconds;
         } else if (seconds < 60) {
-            timeSec.textContent = seconds;
+            timeSec.textContent = String(seconds);
         } else {
             seconds -= 60;
             timeSec.textContent = '0' + seconds;
@@ -38,15 +41,15 @@ const fillGameFieldShirt = (difficult) => {
             if (minutes < 10) {
                 timeMin.textContent = '0' + minutes;
             } else {
-                timeMin.textContent = minutes;
+                timeMin.textContent = String(minutes);
             }
         }
         console.log(seconds);
     };
     const globalTimer = setInterval(timerFunc, 1000);
-    const cardsWrap = document.querySelector('.cards-wrap');
+    const cardsWrap = <HTMLScriptElement>document.querySelector('.cards-wrap');
     cardsWrap.innerHTML = '';
-    let cardAmount = difficult * 6;
+    let cardAmount: number = difficult * 6;
 
     while (cardAmount !== 0) {
         const cardShirt = document.createElement('img');
@@ -56,14 +59,17 @@ const fillGameFieldShirt = (difficult) => {
         cardAmount--;
     }
 
-    const cardsShirt = document.querySelectorAll('.card-shirt');
+    const cardsShirt: NodeListOf<Element> =
+        document.querySelectorAll('.card-shirt');
 
-    let cardOne;
-    let cardTwo;
-    const gameOverBackground = document.querySelector('.game-over-background');
+    let cardOne: string;
+    let cardTwo: string;
+    const gameOverBackground = <HTMLElement>(
+        document.querySelector('.game-over-background')
+    );
     cardsShirt.forEach((card) => {
         card.addEventListener('click', (event) => {
-            const target = event.target;
+            const target = <HTMLElement>event.target;
             const cardIndex = Array.from(cardsShirt).indexOf(target);
             target.setAttribute('src', cardsFinal[cardIndex]);
             if (!cardOne) {
@@ -76,9 +82,11 @@ const fillGameFieldShirt = (difficult) => {
 
                 setTimeout(() => {
                     //Отсюда извлекаем время
-                    const time = document.querySelector('.time');
+                    const time = <HTMLElement>document.querySelector('.time');
                     //Сюда вставляем
-                    const timeAmount = document.querySelector('.time-amount');
+                    const timeAmount = <HTMLElement>(
+                        document.querySelector('.time-amount')
+                    );
                     timeAmount.textContent = time.textContent;
                     gameOverBackground.style.display = 'flex';
                 }, 1200);
@@ -87,15 +95,19 @@ const fillGameFieldShirt = (difficult) => {
 
                 setTimeout(() => {
                     //Отсюда извлекаем время
-                    const time = document.querySelector('.time');
+                    const time = <HTMLElement>document.querySelector('.time');
                     //Сюда вставляем
-                    const timeAmount = document.querySelector('.time-amount');
-                    const gameOverHeader =
-                        document.querySelector('.game-over-header');
+                    const timeAmount = <HTMLElement>(
+                        document.querySelector('.time-amount')
+                    );
+                    const gameOverHeader = <HTMLElement>(
+                        document.querySelector('.game-over-header')
+                    );
                     gameOverHeader.textContent = 'Вы проиграли!';
 
-                    const gameOverImg =
-                        document.querySelector('.game-over-img');
+                    const gameOverImg = <HTMLElement>(
+                        document.querySelector('.game-over-img')
+                    );
                     gameOverImg.setAttribute('src', 'img/lose.png');
 
                     timeAmount.textContent = time.textContent;
@@ -106,8 +118,8 @@ const fillGameFieldShirt = (difficult) => {
     });
 };
 
-let cardsFinal;
-const fillGameFieldCardsOpen = (difficult) => {
+let cardsFinal: [];
+const fillGameFieldCardsOpen = (difficult: number) => {
     const randomCards = cards.sort(() => Math.random() - 0.5);
     const cardsForGame = randomCards.slice(0, difficult * 3);
     const cardsForGameX2 = [];
@@ -115,9 +127,9 @@ const fillGameFieldCardsOpen = (difficult) => {
         cardsForGameX2.push(card);
         cardsForGameX2.push(card);
     }
-    cardsFinal = cardsForGameX2.sort(() => Math.random() - 0.5);
+    cardsFinal: [] = cardsForGameX2.sort(() => Math.random() - 0.5);
 
-    const cardsWrap = document.querySelector('.cards-wrap');
+    const cardsWrap = <HTMLElement>document.querySelector('.cards-wrap');
     for (let card of cardsFinal) {
         const cardOpen = document.createElement('img');
         cardOpen.classList.add('card-open');
@@ -135,7 +147,8 @@ start.addEventListener('click', () => {
 
         request.onload = () => {
             document.body.innerHTML = request.response.body;
-            const newGame = document.querySelectorAll('.new-game');
+            const newGame: NodeListOf<Element> =
+                document.querySelectorAll('.new-game');
             newGame.forEach((el) => {
                 el.addEventListener('click', () => {
                     location.href = 'index.html';
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.ts',
     mode: isProduction ? 'production' : 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -17,11 +17,19 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/',
+            },
+            {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             // { test: /\.json$/, loader: 'json-loader' },
         ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
     },
 
     plugins: [
